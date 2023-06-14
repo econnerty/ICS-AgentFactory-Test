82ef64a3fdba426dc2f95a54b6d7406fbcd0f1b8
Erik Connerty <erikc@email.sc.edu>
2023-06-14 11:26:57 -0400

    initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+#Agent Factory Test File Creator
+#Creates test case files for agent factory
+
+#Author: Erik Connerty
+#Date: 6/14/2023
+#For the USC AI - Institute
+
+import pandas as pd
+import numpy as np
+import constants
+from tqdm.auto import tqdm
+
+
+
